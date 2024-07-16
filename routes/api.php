0b65f27bcc16bf0baca29b75b1131aac9ef57404
linsmar.cruz.ext <linsmar.cruz.ext@infracommerce.com.br>
2024-07-16 14:02:18 -0300

    ✨ Create API Routes

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ScheduleController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('auth', [AuthController::class, 'auth']);
+Route::post('users/create_account', [UserController::class, 'store']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('schedules', ScheduleController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
+});

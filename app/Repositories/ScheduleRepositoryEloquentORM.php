249e64f03eb2e557057ffa7594b9da2a6eaf756d
linsmar.cruz.ext <linsmar.cruz.ext@infracommerce.com.br>
2024-07-17 13:29:07 -0300

    🐛 Fix query to get schedules by date range

--- app/Repositories/ScheduleRepositoryEloquentORM.php
+++ app/Repositories/ScheduleRepositoryEloquentORM.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Schedule;
 use App\Repositories\Abstracts\AbstractRepositoryEloquentORM;
+use Illuminate\Database\Eloquent\Builder;
 
 class ScheduleRepositoryEloquentORM extends AbstractRepositoryEloquentORM
 {
@@ -15,8 +16,12 @@ class ScheduleRepositoryEloquentORM extends AbstractRepositoryEloquentORM
     public function getByDateRange(array $filter): object|null
     {
         $schedules = $this->model->where('user_id', $filter['user']->id)
-            ->whereBetween('start_date', [$filter['start_date'] . ' 00:00:00', $filter['due_date'] . ' 23:59:59'])
-            ->paginate();
-        return $schedules;
+            ->where(function (Builder $query) use ($filter) {
+                $query->whereBetween('start_date', [$filter['start_date'] . ' 00:00:00', $filter['due_date'] . ' 23:59:59'])
+                    ->orwhereBetween('due_date', [$filter['start_date'] . ' 00:00:00', $filter['due_date'] . ' 23:59:59']);
+            })->paginate();
+
+        return $schedules
+        ;
     }
 }
